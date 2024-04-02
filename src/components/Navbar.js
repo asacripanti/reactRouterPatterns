41d332a5f593303628898663e9b83ecd0354e6a3
CarolBarno <carolbarno8@gmail.com>
2024-04-02 16:06:44 +0300

    routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
-import React from "react"
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
-export default function Navbar(){
-    return(
-        <nav>
-            
-        </nav>
-    )
+function Navbar({ dogs }) {
+  const links = dogs.map((dog) => (
+    <NavLink key={dog.name} to={`/dogs/${dog.name.toLowerCase()}`}>
+      {dog.name}
+    </NavLink>
+  ));
+
+  return (
+    <nav>
+      <NavLink to="/dogs" end>
+        Home
+      </NavLink>
+      {links}
+    </nav>
+  );
 }
+
+export default Navbar;
